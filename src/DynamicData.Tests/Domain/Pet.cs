e2e528d76349d243e411557b082936662993c0f9
tjm <tjm@na>
2026-02-15 12:48:13 +0000

    Rearrange file structure to work as a Unity package.

--- src/DynamicData.Tests/Domain/Pet.cs
+++
@@ -1,8 +0,0 @@
-namespace DynamicData.Tests.Domain;
-
-public class Pet
-{
-    public string? Animal { get; set; }
-
-    public string? Name { get; set; }
-}
--- src/DynamicData.Tests/Issues/Readme.txt
+++
@@ -1,3 +0,0 @@
-﻿This is a dumping ground for issue replication
-
-I am happy for these to be checked in until tests are moved to a more appropriate place.
